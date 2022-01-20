12182bedd60554cad482a1481fbfbd885544fe00
mahaManae <johndoe@example.com>
2022-01-20 11:56:13 +0300

    b

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ app.use(qoranRoute);
 ////////////////////
 console.log(process.env.PORT);
 
-const Port = 5000;
 app.listen(process.env.PORT, () => {
   console.log("server run on 5000 port");
 });
